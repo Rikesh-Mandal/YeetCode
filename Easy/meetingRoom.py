3b752e236a64b09862043b4e9bc7b3e96472283d
Rikesh Mandal <rikesh.mandal26@gmail.com>
2026-02-01 02:30:00 +0000

    added the meetingRoom I problem

---
+++ Easy/meetingRoom.py
@@ -0,0 +1,8 @@
+def meetingRoom(intervals: list[list[int]]) -> bool:
+    intervals.sort(key= lambda x:x[0])
+    for i in range(1,len(intervals)):
+        if intervals[i][0] < intervals[i-1][1]:
+            return False
+    return True
+
+print(f"All meetings attendable?: {meetingRoom([[0,30], [29,20], [31,30]])}")
